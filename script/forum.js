1935e1e5dfd1cd92990610052e238e264e826251
101beardo <101tash101@gmail.com>
2022-10-02 14:11:12 +0530

    dayf

---
+++ script/forum.js
@@ -0,0 +1,6 @@
+import navbar from "../components/navbar.js";
+let nav = document.getElementById("navbar");
+nav.innerHTML = navbar();
+import footer from "../components/footer.js";
+let foot = document.getElementById("footer");
+foot.innerHTML = footer();
